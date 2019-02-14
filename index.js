952b63c0c6a8189d043d366d3a0f21499d610ccd
mosterbox <romapotapov7@gmail.com>
2019-02-14 16:03:27 +0200

    removed TRIGGERS and constants

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-import * as constants from './constants';
+import Az from 'az';
+
+const TOKEN = '774748306:AAE2A9rSYhExCzTH4RoUYD_OUsx66rxBtis';
 
 const TelegramBot = require('node-telegram-bot-api');
 
 // Включить опрос сервера
-const bot = new TelegramBot(constants.TOKEN, {polling: true});
+const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, `Hello from HEROKU, ${msg.from.first_name}!!`);
 })
+
