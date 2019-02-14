511074b0d619c3301466f9de22cf12a4522e29a2
mosterbox <romapotapov7@gmail.com>
2019-02-14 15:19:16 +0200

    add command

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const token = '774748306:AAE2A9rSYhExCzTH4RoUYD_OUsx66rxBtis';
 // Включить опрос сервера
 const bot = new TelegramBot(token, {polling: true});
 
-var notes = [];
-
-bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
-    var userId = msg.from.id;
-    var text = match[1];
-    var time = match[2];
-
-    notes.push({ 'uid': userId, 'time': time, 'text': text });
-
-    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
-});
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, `Hello from HEROKU, ${msg.from.first_name}!!`);
+})
