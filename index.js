59ba04af885862b7f5be882928e3ecaa368c0679
mosterbox <romapotapov7@gmail.com>
2019-02-14 16:04:14 +0200

    removed TRIGGERS and constants

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import Az from 'az';
-
 const TOKEN = '774748306:AAE2A9rSYhExCzTH4RoUYD_OUsx66rxBtis';
 
 const TelegramBot = require('node-telegram-bot-api');
@@ -9,5 +7,4 @@ const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, `Hello from HEROKU, ${msg.from.first_name}!!`);
-})
-
+});
